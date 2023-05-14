6d5e34e6e07fc936fd298343b5b7ed97b0e6aca9
Noor Alam <nooralamjaturi@gmail.com>
2023-05-14 14:51:40 -0500

    Updates

--- src/api.js
+++
@@ -1,5 +0,0 @@
-export const getMovieById = async (id) => {
-  const response = await fetch(`https://www.omdbapi.com/?apikey=f8586019&i=${id}`);
-  const data = await response.json();
-  return data;
-};
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const fetchMovie = async (id) => {
-  const response = await fetch(`https://www.omdbapi.com/?apikey=f8586019&i=${id}`);
-  const data = await response.json();
-  if (data.Error) {
-    throw new Error(data.Error);
-  }
-  return data;
-};
